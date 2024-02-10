0366a689f340fff5e387e44ab0b7e8835c0f991e
Jason Stajich <jasonstajich.phd@gmail.com>
2024-02-09 17:13:18 -0800

    fix cpus

--- 16S/pipeline/01_ASV_16S.sh
+++ 16S/pipeline/01_ASV_16S.sh
@@ -3,30 +3,32 @@
 #SBATCH --time=12:00:00   # walltime
 #SBATCH -N 1 -n 1 -c 48
 #SBATCH --mem=48gb # memory per CPU core
-#SBATCH --out logs/ASV_NCBI_vsearch.%A.log
+#SBATCH --out logs/ASV_vsearch.%A.log
+
 
 CPU=$SLURM_CPUS_ON_NODE # set the CPUS dynamicall for the job
 if [ -z $CPU ]; then # unless this is not really a slurm job
  CPU=2 # set the number of CPUs to 2
 fi
 
+
 module load amptk
 module load usearch
 
-BASE=ECDRE_Y0_1_NCBI
+BASE=ECDRE_Y0_1_16S_ASV
 
 #Change this to match your data folder name
 INPUT=input
 
 #Pre-processing steps will use `amptk illumina` command for demultiplexed PE reads
 if [ ! -f $BASE.demux.fq.gz ]; then
-	amptk illumina -i $INPUT --merge_method vsearch -f ITS1-F -r ITS2 --require_primer off -o $BASE --usearch usearch9  --rescue_forward on --primer_mismatch 2 -l 250
+	amptk illumina -i $INPUT --merge_method vsearch -f 515FB -r 806RB --require_primer off -o $BASE --usearch usearch9 --cpus $CPU --rescue_forward on --primer_mismatch 2 -l 300
 fi
 
 if [ ! -f $BASE.ASV_table.txt ];  then
- amptk dada2 -i $BASE.demux.fq.gz -o $BASE --uchime_ref ITS --usearch usearch9 -e 0.9
+ 	amptk dada2 -i $BASE.demux.fq.gz -o $BASE --uchime_ref 16S --usearch usearch9 -e 0.9 --cpus $CPU
 fi
 
 if [ ! -f $BASE.taxonomy.txt ]; then
- amptk taxonomy -f $BASE.dada2.ASV.fa -i $BASE.ASV_table.txt -d FungiITSrefseq
+	amptk taxonomy -f $BASE.dada2.ASV.fa -i $BASE.dada2.asv_table.txt -d 16S
 fi
