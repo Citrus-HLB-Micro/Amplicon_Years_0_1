c7e113c9ad643e005ace3668a4f641e291421be3
Jason Stajich <jasonstajich.phd@gmail.com>
2024-12-20 08:14:26 -0800

    16S results

--- CA_only/16S/pipeline/01_ASV_16S.sh
+++ CA_only/16S/pipeline/01_ASV_16S.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --time=14-0:00:00   # walltime
 #SBATCH -N 1 -n 1 -c 48
-#SBATCH --mem=48gb # memory per CPU core
+#SBATCH --mem=256gb # memory per CPU core
 #SBATCH --out logs/ASV_vsearch.%A.log
 
 
@@ -15,7 +15,7 @@ fi
 module load amptk
 module load usearch
 
-BASE=ECDRE_Y0_1_16S_ASV_CAonly_202404
+BASE=ECDRE_Y0_1_16S_ASV_CAonly_20240501
 
 #Change this to match your data folder name
 INPUT=input
@@ -25,10 +25,11 @@ if [ ! -f $BASE.demux.fq.gz ]; then
 	amptk illumina -i $INPUT --merge_method vsearch -f 515FB -r 806RB --require_primer off -o $BASE --usearch usearch9 --cpus $CPU --rescue_forward on --primer_mismatch 2 -l 300
 fi
 
-if [ ! -f $BASE.ASV_table.txt ];  then
+if [ ! -f $BASE.otu_table.txt ];  then
  	amptk dada2 -i $BASE.demux.fq.gz -o ${BASE} --uchime_ref 16S --usearch usearch9 -e 0.9 --cpus $CPU
 fi
 
 if [ ! -f $BASE.taxonomy.txt ]; then
-	amptk taxonomy -f $BASE.dada2.ASV.fa -i $BASE.dada2.asv_table.txt -d 16S
+	amptk taxonomy -f $BASE.ASVs.fa -i $BASE.otu_table.txt -d 16S
+	amptk taxonomy -f $BASE.cluster.otus.fa -i $BASE.cluster.otu_table.txt -d 16S
 fi
